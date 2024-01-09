1c5da299838bf2437fc2204c13fd519ba49c1124
Matteus Varlesse <Varlesse04@gmail.com>
2024-01-08 21:36:06 -0300

    feat: finished styles

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,15 +2,17 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './_layouts/app'
 import { AuthLayout } from './_layouts/auth'
-import { Dashboard } from './app/dashboard'
+import { NotFound } from './404'
+import { SignIn } from './app/auth/sign-in'
+import { SignUp } from './app/auth/sign-up'
+import { Dashboard } from './app/dashboard/dashboard'
 import { Orders } from './app/orders/orders'
-import { SignIn } from './auth/sign-in'
-import { SignUp } from './auth/sign-up'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
